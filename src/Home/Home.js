8c0b6fabccc1e58fbde6c2697ec78bd3bb9585a4
alexsandro <alexsandro.nv02@gmail.com>
2021-11-07 19:59:30 -0300

    iniciado page home

---
+++ src/Home/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Text, StyleSheet, Platform, Image } from "react-native";
+
+
+export function Home(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.titleHome}>Pricer</Text>
+            <View style={styles.logoHome}>
+                <Image source={require('../assets/logo.png')} />
+            </View>
+            <Text style={styles.iniciarButton}>Inicial</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#FFE600',
+    },
+    titleHome:{
+        fontSize:24,
+        color:'#2D3277'
+    },
+    logoHome:{
+        marginTop:30
+    },
+    iniciarButton:{
+        backgroundColor:'#2D3277', padding:10,color:'#fff',fontWeight:'bold',
+            fontSize:18,marginTop:80,borderRadius:10
+    }
+
+})
